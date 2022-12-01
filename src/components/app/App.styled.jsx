8c5597252b7c527bad8d307d9d7441d8284a239b
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-01 21:54:03 +0200

    add brakepoints and basic bg

--- src/components/app/App.styled.jsx
+++ src/components/app/App.styled.jsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import backgroundBot from '../../images/background/Ellipse1.png';
+import backgroundTop from '../../images/background/Ellipse2.png';
 
 export const AppWrapper = styled.div`
   height: 100vh;
@@ -8,13 +10,20 @@ export const AppWrapper = styled.div`
   align-items: center;
   color: '#010101';
   font-size: 10px;
-  background-color: aqua;
+
   @media (min-width: 768px) {
     font-size: 20px;
-    background-color: orangered;
+    background-image: url(${backgroundBot}), url(${backgroundTop});
+    background-position: left bottom, right top;
+    background-position-x: -146px, 402px;
+    background-position-y: 716px, -117px;
+    background-repeat: no-repeat, no-repeat;
+    background-size: 630px 486px;
   }
   @media (min-width: 1280px) {
     font-size: 40px;
-    background-color: #62ee62;
+
+    background-position-x: -143px, 732px;
+    background-position-y: 395px, -122px;
   } ;
 `;
