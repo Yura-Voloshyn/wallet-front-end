217798f136d5d9f9327c85aa2b1894629092ba23
OlenaLotos <olenameshanych@gmail.com>
2022-12-02 13:21:43 +0200

    fix small

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
 
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* </Provider> */}
   </React.StrictMode>
 );
