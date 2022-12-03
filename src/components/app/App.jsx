dcfc12455fc16566da0d7a7468104604a9eae384
Ihor Khomenko <igorhomenko97@gmail.com>
2022-12-03 16:19:16 +0200

    add chenges

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,9 @@
 import { AppWrapper } from './App.styled';
+import Home from 'pages/home';
 
 const App = () => {
-  return <AppWrapper>Wallet front part</AppWrapper>;
+  return <AppWrapper>
+    <Home/>
+  </AppWrapper>;
 };
 export default App;
