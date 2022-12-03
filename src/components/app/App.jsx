a0bb90950726afac0cfb57b917bdb70ed964e8e7
OlenaLotos <olenameshanych@gmail.com>
2022-12-03 20:17:28 +0200

    some edit login page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,13 @@
 import { AppWrapper } from './App.styled';
+// import SubmitBtn from '../Button/SubmitBtn';
+
+// import LoginPage from './../../pages/LoginPage/LoginPage';
 export const App = () => {
-  return <AppWrapper>Wallet front end part</AppWrapper>;
+  return (
+    <AppWrapper>
+      {/* <SubmitBtn /> */}
+      {/* <LoginPage /> */}
+      Wallet front end part
+    </AppWrapper>
+  );
 };
