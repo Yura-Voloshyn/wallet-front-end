858ce2f15f8a8eae157c89e44a5e04a058656edb
Kateryna Yankovenko <k.yankovenko@ukr.net>
2022-12-04 17:46:13 +0100

    added transaction types for modalAddTrs

---
+++ src/components/modalAddTransaction/transactionTypes.js
@@ -0,0 +1,17 @@
+export const expenceTransaction = [
+  { value: "Main expenses", label: "Main expenses" },
+  { value: "Products", label: "Products" },
+  { value: "Car", label: "Car" },
+  { value: "Self care", label: "Self care" },
+  { value: "Child care", label: "Child care" },
+  { value: "Household products", label: "Household products" },
+  { value: "Education", label: "Education" },
+  { value: "Leisure", label: "Leisure" },
+  { value: "Other expenses", label: "Other expenses" },
+  { value: "Entertainment", label: "Entertainment" },
+];
+
+export const incomeTransaction = [
+  { value: "Regular income", label: "Regular income" },
+  { value: "Irregular income", label: "Irregular income" },
+];
