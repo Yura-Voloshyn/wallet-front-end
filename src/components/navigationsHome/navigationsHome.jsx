a359fbc304470a78366a99866c41d448b7579b91
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-05 09:20:08 +0200

    conflict path fix

--- src/components/navigationsHome/navigationsHome.jsx
+++ src/components/navigationsHome/navigationsHome.jsx
@@ -1,45 +1,44 @@
-import home from "../../icons/home.svg"
-import stat from "../../icons/stat.png"
-import styled from '@emotion/styled'
+import home from '../../images/icons/home_icon.svg';
+import stat from '../../images/icons/diagram_icon.svg';
+import styled from '@emotion/styled';
 
 const Icons = styled.img`
-    display: inline-block;
-    width: 18px;
-    margin-right: 23px;
-    margin-left: 3px;
-`
+  display: inline-block;
+  width: 18px;
+  margin-right: 23px;
+  margin-left: 3px;
+`;
 const Link = styled.a`
-    font-family: 'Poppins';
-    font-style: normal;
-    font-size: 18px;
-    line-height: 27px;
-    text-decoration: none;
-    color: #000000;
-`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-size: 18px;
+  line-height: 27px;
+  text-decoration: none;
+  color: #000000;
+`;
 const Ul = styled.ul`
-    margin-top: 40px;
-`
-
+  margin-top: 40px;
+`;
 
 const NavigationsHome = () => {
-
-    return (
-        <div>
-            <Ul>
-                <li>
-                    <Link href="/">
-                    <Icons src={home} alt=""/>   
-                        Home</Link>
-                </li>
-                <li>
-                    
-                    <Link href="/">
-                    <Icons src={stat} alt=""/> 
-                        Statictics</Link></li>
-            </Ul>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <Ul>
+        <li>
+          <Link href="/">
+            <Icons src={home} alt="" />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <Icons src={stat} alt="" />
+            Statictics
+          </Link>
+        </li>
+      </Ul>
+    </div>
+  );
+};
 
 export default NavigationsHome;
