8e697a5a657679fb1b3811e5eafb03776d46967c
IrynaShashko <irishkashashko@gmail.com>
2022-12-06 15:55:41 +0200

    done progresBar

--- src/components/RegistrationForm/RegistrationForm.styled.jsx
+++ src/components/RegistrationForm/RegistrationForm.styled.jsx
@@ -1,27 +1,34 @@
 import styled from '@emotion/styled';
 import { Form, Field } from 'formik';
-import { Link } from 'react-router-dom';
 import { ReactComponent as email } from '../../images/icons/email.svg';
 import { ReactComponent as password } from '../../images/icons/password.svg';
 import { ReactComponent as user } from '../../images/icons/user.svg';
 
 export const Div = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #fff;
-  max-width: 480px;
+  width: 100vw;
   height: 100vh;
+
+  @media screen and (min-width: 768px) {
+    width: 533px;
+    height: 616px;
+    border-radius: 20px;
+  }
 `;
 
 export const FormStyled = styled(Form)`
-  padding: 20px 0;
+  margin-top: 60px;
   background-color: #fff;
 `;
 
 export const FormLabel = styled.label`
   position: relative;
   display: block;
+  box-sizing: content-box;
   margin-bottom: 40px;
 `;
 
@@ -34,60 +41,6 @@ export const FormField = styled(Field)`
   border: none;
   padding-left: 42px;
   border-bottom: 1px solid #e0e0e0; ;
-`;
-
-export const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 280px;
-`;
-
-export const NavButton = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 280px;
-  height: 50px;
-  background: #24cca7;
-  border-radius: 20px;
-  font-family: 'Circe';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 27px;
-  text-align: center;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  margin-bottom: 20px;
-  background: #ffffff;
-  border: 1px solid #4a56e2;
-  border-radius: 20px;
-  color: rgba(74, 86, 226, 1);
-  &:active {
-    background: #24cca7;
-  }
-`;
-export const NavButtonR = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 280px;
-  height: 50px;
-  background: #24cca7;
-  border-radius: 20px;
-  font-family: 'Circe';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 27px;
-  text-align: center;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  margin-bottom: 20px;
-  color: #fff;
-  background: #24cca7;
 `;
 
 export const IconEmail = styled(email)`
@@ -115,15 +68,6 @@ export const IconUser = styled(user)`
   width: 18px;
   height: 18px;
   fill: #e0e0e0;
-`;
-
-export const DivLine = styled.div`
-  width: 280px;
-  height: 0px;
-  left: 20px;
-  top: 306px;
-
-  border: 4px solid #e5f1ef;
 `;
 
 export const ErrorText = styled.p`
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,11 +1,22 @@
 import RegistrationForm from 'components/RegistrationForm';
-// import { PageContainer } from '../RegistrationPage/RegistrationPage.styled';
+import { useMedia } from 'react-use';
 
 const RegistrationPage = () => {
+  const isMobile = useMedia('(max-width: 767px)');
+  const isTablet = useMedia('(min-width: 768px) and (max-width: 1279px)');
+  const isDesktop = useMedia('(min-width: 1280px)');
+
   return (
-    <div>
-      <RegistrationForm />
-    </div>
+    <>
+      {isMobile ? <RegistrationForm /> : null}
+      {isTablet ? (
+        <div>
+          <h1>Finance App</h1>
+          <RegistrationForm />
+        </div>
+      ) : null}
+      {isDesktop ? <h1>booooo</h1> : null}
+    </>
   );
 };
 
