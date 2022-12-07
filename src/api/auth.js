ee7f82dba60be078c5b655adb8b22718eee523c4
IrynaShashko <irishkashashko@gmail.com>
2022-12-07 15:07:09 +0200

    add token

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,17 +4,19 @@ const instance = axios.create({
   baseURL: 'https://wallet-jet.vercel.app/',
 });
 
-// const setToken = token => {
-//   if (token) {
-//     return (instance.defaults.headers.common.authorization = `Bearer ${token}`);
-//   }
-//   instance.defaults.headers.common.authorization = '';
-// };
+const setToken = token => {
+  console.log('token', token);
+  if (token) {
+    return (instance.defaults.headers.common.authorization = `Bearer ${token}`);
+  }
+  instance.defaults.headers.common.authorization = '';
+};
 
 export const signup = async data => {
   console.log('data api', data);
   const { data: result } = await instance.post('/api/auth/register', data);
-  //   setToken(result.token);
+  console.log('result.data.token', result.data.token);
+  setToken(result.data.token);
   return result;
 };
 
