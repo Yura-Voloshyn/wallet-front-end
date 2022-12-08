c3b54ea333bddf7be3d2edbb458444e01c27a1d1
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-08 20:38:01 +0200

    add basename-wallet to index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="wallet">
           <App />
         </BrowserRouter>
       </PersistGate>
