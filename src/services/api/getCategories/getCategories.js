dcc7cb7a67931f5b2225e5df4b96c11543ce5f47
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-10 00:19:35 +0200

    small fix

--- src/services/api/getCategories/getCategories.js
+++ src/services/api/getCategories/getCategories.js
@@ -1,11 +1,9 @@
 // import axios from 'axios';
 import instance from '../auth';
-// const CATERGORIES_URL =
-//   'https://wallet-jet.vercel.app/api/transactions/categories';
 
 export const getCategories = async () => {
-  const { data } = await instance.get('api/transactions/categories');
+  const { data } = await instance.get('/api/transactions/categories');
   return data;
 };
 
-// export default getCategories;
+export default getCategories;
