b61270305ded81375c65b6ee216d9d8f0e62c3cf
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-10 19:21:56 +0200

    conflict fix

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,50 +1,42 @@
-import TableHead from "./TableHead";
-import TableBody from "./TableBody";
-import {TableStyle, TableText} from "./Table.styled"
-import TableMobile from "./TableMobile";
+import TableHead from './TableHead';
+import TableBody from './TableBody';
+import { TableStyle, TableText } from './Table.styled';
+import TableMobile from './TableMobile';
 
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { fetchTransactions } from "redux/transaction/transactionOperation";
-import { useDispatch } from "react-redux";
-
-
+import { fetchTransactions } from 'redux/transaction/transactionOperation';
+import { useDispatch } from 'react-redux';
 
 const Table = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchTransactions());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
 
-    const { transactions } = useSelector(state => state.transactions)
+  const { transactions } = useSelector(state => state.transactions);
 
-    return (
+  return (
+    <>
+      {/* {console.log(transactions)} */}
+      {transactions.length === 0 ? (
+        <TableStyle>
+          <TableHead />
+          <TableText>Sorry, you havn't transactions</TableText>
+        </TableStyle>
+      ) : (
         <>
-            {/* {console.log(transactions)} */}
-            {transactions.length === 0
-            ? (
-                <TableStyle>
-                    <TableHead/>
-                    <TableText>Sorry, you havn't transactions</TableText>
-                </TableStyle>)
-            : (
-                <>
-                <TableStyle>
-                    <TableHead/>
-                    <TableBody items={transactions}/>   
-                </TableStyle>
-                    <TableMobile items={transactions}/>
-                </>
-            )}
-           
-            
+          <TableStyle>
+            <TableHead />
+            <TableBody items={transactions} />
+          </TableStyle>
+          <TableMobile items={transactions} />
         </>
-
-
-    )
-}
-
+      )}
+    </>
+  );
+};
 
 export default Table;
