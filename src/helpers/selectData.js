8230e88541a6e61225dfff4bc9c6b52eed4b42f1
Slava Grobovoy <slava.grobovoy27@ukr.net>
2022-12-11 19:12:03 +0200

    added a lock on input to Select and added a calculation for years

--- src/helpers/selectData.js
+++ src/helpers/selectData.js
@@ -12,12 +12,15 @@ export const MONTH = [
   { label: 'November', value: '11' },
   { label: 'December', value: '12' },
 ];
-export const YEAR = [
-  { label: '2016', value: '2016' },
-  { label: '2017', value: '2017' },
-  { label: '2018', value: '2018' },
-  { label: '2019', value: '2019' },
-  { label: '2020', value: '2020' },
-  { label: '2021', value: '2021' },
-  { label: '2022', value: '2022' },
-];
+
+let maxOffset = 6;
+let thisYear = new Date().getFullYear();
+let YEARS = [];
+for (let i = 0; i <= maxOffset; i++) {
+  const year = thisYear - i;
+  YEARS.push({
+    label: `${year}`,
+    value: `${year}`,
+  });
+}
+export { YEARS }
