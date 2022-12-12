03cefb45a0438a800ef2242cec49232bceec7a26
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-12 13:31:43 +0200

    refresh and add addTransaction modal

--- src/services/api/transactios/api.jsx
+++ src/services/api/transactios/api.jsx
@@ -1,7 +1,7 @@
 import instance from '../auth';
 
 export const getTransactions = async () => {
-  const data = await instance.get('/api/trasactions');
+  const data = await instance.get('/api/transactions');
   return data;
 };
 
