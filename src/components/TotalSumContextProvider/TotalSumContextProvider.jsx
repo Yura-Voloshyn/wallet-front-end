7996eb2e9662ae974d0df2adab13001efaa663a7
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-12-14 14:49:32 +0200

    deploy fix

--- src/components/TotalSumContextProvider/TotalSumContextProvider.jsx
+++ src/components/TotalSumContextProvider/TotalSumContextProvider.jsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-
 const TotalSumContext = createContext({});
 
 export const TotalSumContextProvider = ({ children }) => {
@@ -10,14 +9,9 @@ export const TotalSumContextProvider = ({ children }) => {
   const { transactions } = useSelector(state => state.transactions);
   console.log(transactions);
   useEffect(() => {
-    transactions.map(res => {
-      console.log(res.balance);
-    })
     return transactions.length === 0
       ? setBalance('0,00')
-      : setBalance(
-          `₴ ${transactions[0].balance.toFixed(2)}` 
-        );
+      : setBalance(`₴ ${transactions[0].balance.toFixed(2)}`);
   }, [dispatch, transactions]);
 
   return (
